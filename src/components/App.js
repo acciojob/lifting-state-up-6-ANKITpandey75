3f46c7a3dafecf1a6001bc9eb70e9714e812e311
Ankit Kumar <er.ankitkumar75@gmail.com>
2026-07-26 15:17:34 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+import "./../styles/App.css";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a React app", completed: false },
+    { id: 3, text: "Deploy the React app", completed: false },
+  ]);
+
+  const handleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: true }
+          : todo
+      )
+    );
+  };
+
+  return (
+    <div>
+      <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
+    </div>
+  );
+}
+
+export default App;
